eb3bd414f5e62420a7f000a6071252f88bc76a21
RahimALi12 <141006532+RahimALi12@users.noreply.github.com>
2026-05-11 10:38:31 +0500

    Update userApi.js

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -1,7 +1,7 @@
 // import axios from 'axios';
 import axios from '../../utils/axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api/users' });
+const API = axios.create({ baseURL: '/api/users' });
 
 export const register = (data) => API.post('/register', data);
 export const login = (data) => API.post('/login', data);
